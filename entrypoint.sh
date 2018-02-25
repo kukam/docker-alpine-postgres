0ee91aa6cc691cb055d44757aa340b77619ce739
kukam <dusan.grmela@gmail.com>
2018-02-25 11:08:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,30 @@
 #!/bin/sh
+
+mkdir /run/postgres
+chown postgres:postgres /run/postgres
 chown -R postgres "$PGDATA"
 
 if [ -z "$(ls -A "$PGDATA")" ]; then
     gosu postgres initdb
     sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
-
-    : ${POSTGRES_USER:="postgres"}
-    : ${POSTGRES_DB:=$POSTGRES_USER}
-
-    if [ "$POSTGRES_PASSWORD" ]; then
-      pass="PASSWORD '$POSTGRES_PASSWORD'"
-      authMethod=md5
-    else
-      echo "==============================="
-      echo "!!! Use \$POSTGRES_PASSWORD env var to secure your database !!!"
-      echo "==============================="
-      pass=
-      authMethod=trust
-    fi
-    echo
-
-
-    if [ "$POSTGRES_DB" != 'postgres' ]; then
-      createSql="CREATE DATABASE $POSTGRES_DB;"
-      echo $createSql | gosu postgres postgres --single -jE
-      echo
-    fi
-
-    if [ "$POSTGRES_USER" != 'postgres' ]; then
-      op=CREATE
-    else
-      op=ALTER
-    fi
-
-    userSql="$op USER $POSTGRES_USER WITH SUPERUSER $pass;"
-    echo $userSql | gosu postgres postgres --single -jE
-    echo
-
     { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA"/pg_hba.conf
+fi
+    
+: ${POSTGRES_ADMIN_PASS:="postgres"}
+: ${POSTGRES_USER:="postgres"}
+: ${POSTGRES_USER_PASS:=$POSTGRES_USER}
+: ${POSTGRES_DB:=$POSTGRES_USER}
+
+echo "ALTER USER postgres WITH PASSWORD '$POSTGRES_ADMIN_PASS';" | gosu postgres postgres --single -jE
+echo   
+
+if [ "$POSTGRES_USER" != 'postgres' ]; then
+    gosu postgres createuser $POSTGRES_USER
+    gosu postgres createdb $POSTGRES_DB
+    echo "ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_USER_PASS';" | gosu postgres postgres --single -jE
+    echo
+    echo "GRANT ALL PRIVILEGES ON DATABASE '$POSTGRES_DB' TO '$POSTGRES_USER' ;
+    echo
 fi
 
 exec gosu postgres "$@"
