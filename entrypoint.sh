b1950bd81a7ecb7767682282496795206e4895df
kukam <dusan.grmela@gmail.com>
2018-02-26 19:43:40 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,18 +15,18 @@ if [ -z "$(ls -A "$PGDATA")" ]; then
     { echo; echo "host  all  all  ::1/128  md5"; } >> "$PGDATA"/pg_hba.conf
     { echo; echo "host  all  all  0.0.0.0/0  md5"; } >> "$PGDATA"/pg_hba.conf
 
-    : ${POSTGRES_ADMIN_PASS:="postgres"}
+    : ${POSTGRES_ADMIN_PWD:="postgres"}
     : ${POSTGRES_USER:="postgres"}
-    : ${POSTGRES_USER_PASS:=$POSTGRES_USER}
+    : ${POSTGRES_USER_PWD:=$POSTGRES_USER}
     : ${POSTGRES_DB:=$POSTGRES_USER}
 
-    echo "ALTER USER postgres WITH PASSWORD '$POSTGRES_ADMIN_PASS';" | gosu postgres postgres --single -jE
+    echo "ALTER USER postgres WITH PASSWORD '$POSTGRES_ADMIN_PWD';" | gosu postgres postgres --single -jE
     echo   
 
     if [ "$POSTGRES_USER" != 'postgres' ]; then
         echo "CREATE DATABASE $POSTGRES_DB;" | gosu postgres postgres --single -jE
         echo
-        echo "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_USER_PASS';" | gosu postgres postgres --single -jE
+        echo "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_USER_PWD';" | gosu postgres postgres --single -jE
         echo
         echo "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;" | gosu postgres postgres --single -jE
         echo
