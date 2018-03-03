92311cef0cbe293e7e3419c3c83ceb0bfebf5612
kukam <dusan.grmela@gmail.com>
2018-03-03 19:07:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ chown postgres:postgres /run/postgresql
 chown -R postgres "$PGDATA"
 
 # parameters
-POSTGRES_ADMIN_PWD = ${DB_ADMIN_PASSWORD:="postgres"}
-POSTGRES_USER = ${DB_USER:="postgres"}
-POSTGRES_USER_PWD = ${DB_USER_PASSWORD:=$POSTGRES_USER}
-POSTGRES_DB = ${DB_NAME:=$POSTGRES_USER}
+: ${DB_ADMIN_PASSWORD:="postgres"}
+: ${DB_USER:="postgres"}
+: ${DB_USER_PASSWORD:=postgres}
+: ${DB_NAME:=postgres}
 
 if [ -z "$(ls -A "$PGDATA")" ]; then
 
@@ -21,15 +21,18 @@ if [ -z "$(ls -A "$PGDATA")" ]; then
     { echo; echo "host  all  all  ::1/128  md5"; } >> "$PGDATA"/pg_hba.conf
     { echo; echo "host  all  all  0.0.0.0/0  md5"; } >> "$PGDATA"/pg_hba.conf
 
-    echo "ALTER USER postgres WITH PASSWORD '$POSTGRES_ADMIN_PWD';" | gosu postgres postgres --single -jE
+    echo "ALTER USER postgres WITH PASSWORD '$DB_ADMIN_PASSWORD';" | gosu postgres postgres --single -jE
     echo   
 
-    if [ "$POSTGRES_USER" != 'postgres' ]; then
-        echo "CREATE DATABASE $POSTGRES_DB;" | gosu postgres postgres --single -jE
+    if [ "$DB_NAME" != 'postgres' ]; then
+        echo "CREATE DATABASE $DB_NAME;" | gosu postgres postgres --single -jE
         echo
-        echo "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_USER_PWD';" | gosu postgres postgres --single -jE
+    fi
+
+    if [ "$DB_USER" != 'postgres' ]; then
+        echo "CREATE USER $DB_USER WITH PASSWORD '$DB_USER_PASSWORD';" | gosu postgres postgres --single -jE
         echo
-        echo "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;" | gosu postgres postgres --single -jE
+        echo "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;" | gosu postgres postgres --single -jE
         echo
     fi
 fi
